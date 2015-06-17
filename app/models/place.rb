844e1df747c36c454f9ca6f6b2be4b4dffba97f1
Tae Kim <taekimsf@gmail.com>
2015-06-17 15:43:35 -0700

    fixed place model problem

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,6 +1,5 @@
 class Place < ActiveRecord::Base
 	has_many :posts 
 
-	validates_length_of :title, :maximum => 100
-	validates_length_of :body, :maximum => 1000
+	validates :name, presence: true
 end
