895a54e2140d07a0c7446b7595334715a5247a3a
Tae Kim <taekimsf@gmail.com>
2015-06-18 09:57:24 -0700

    blah

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,17 +9,17 @@
 User.delete_all
 Post.delete_all
 
-User.create(first_name: 'John', last_name: 'Doe', email: 'john@doe.com', password: 'test', current_city: 'San Francisco')
-User.create(first_name: 'Jane', last_name: 'Doe', email: 'jane@doe.com', password: 'test', current_city: 'Los Angeles')
+u1 = User.create(first_name: 'John', last_name: 'Doe', email: 'john@doe.com', password: 'test', current_city: 'San Francisco')
+u2 = User.create(first_name: 'Jane', last_name: 'Doe', email: 'jane@doe.com', password: 'test', current_city: 'Los Angeles')
 
-Post.create(title: 'Hello, world!', body: 'This is my very first post.')
-Post.create(title: 'Hello, again!', body: 'This is my second post.')
-Post.create(title: 'Hello, for the third time!', body: 'This is my third post and it is
+pt1 = Post.create(title: 'Hello, world!', body: 'This is my very first post.')
+pt2 = Post.create(title: 'Hello, again!', body: 'This is my second post.')
+pt3 = Post.create(title: 'Hello, for the third time!', body: 'This is my third post and it is
 much longer so that I can check and see if I should add any limits to
 the post length. For example, if I was someone who loved to write and write
 and write, would that be a problem? Would you still love me? Would you wish
 that I was not quite so verbose? I would love to know your real thoughts.')
 
-Place.create(name: 'London')
-Place.create(name: 'Gibraltar')
-Place.create(name: 'San Francisco')
+p1 = Place.create(name: 'London')
+p2 = Place.create(name: 'Gibraltar')
+p3 = Place.create(name: 'San Francisco')
